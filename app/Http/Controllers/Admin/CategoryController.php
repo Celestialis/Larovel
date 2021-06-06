46ebfb35ea18e9243d40160124449a5111ee79d2
Celestialis <nagibatorkir@gmail.com>
2021-06-06 22:29:32 +0700

    1

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\News;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -14,7 +16,11 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('admin.categories.index');
+        $model = new Category();
+        $categories = $model->categoryList();
+        return view('admin.categories.index',[
+            'categories' => $categories
+        ]);
     }
 
     /**
