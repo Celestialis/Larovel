911aeb71207bb946ab1d60ab6271ed67769a9dd1
Celestialis <nagibatorkir@gmail.com>
2021-06-23 16:01:12 +0700

    Хранение файлов. Очередь.

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\NewsCreate;
 use App\Models\Category;
 use App\Models\News;
+use App\Services\FileUploadService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
+
 class NewsController extends Controller
 {
 	/**
@@ -39,21 +42,20 @@ class NewsController extends Controller
 	 *
 	 * @param \Illuminate\Http\Request $request
 	 * @return \Illuminate\Http\RedirectResponse
+	 * @throws \Exception
 	 */
-	public function store(NewsCreate $request)
+	public function store(NewsCreate $request, FileUploadService $uploadedService): \Illuminate\Http\RedirectResponse
 	{
 		$fields = $request->validated();
 		$fields['slug'] = \Str::slug($fields['title']);
+		$fields['image'] = $uploadedService->upload($request);
 
 		$news = News::create($fields);
 		if ($news) {
 			return redirect()->route('news.index');
 		}
-
 		return back()->withInput();
-
 	}
-
 	/**
 	 * Display the specified resource.
 	 *
@@ -84,17 +86,23 @@ class NewsController extends Controller
 	 * @param News $news
 	 * @return \Illuminate\Http\Response
 	 */
-	public function update(Request $request, News $news)
+	public function update(Request $request, News $news, FileUploadService $uploadedService)
 	{
 		$request->validate([
 			'title' => ['required']
 		]);
+
 		$fields = $request->only('category_id', 'title', 'description', 'image', 'status');
 		$fields['slug'] = \Str::slug($fields['title']);
+		$fields['image'] = $uploadedService->upload($request);
+
 		$news = $news->fill($fields)->save();
 		if ($news) {
-			return redirect()->route('news.index');
+			return redirect()->route('news.index')
+				->with('success', 'Запись успешно обновлена');
 		}
+
+
 		return back();
 	}
 	/**
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -15,6 +15,9 @@
                 @forelse ($newsList as $news)
                 <div class="col">
                     <div class="card shadow-sm">
+                        @if($news->image)
+                            <img src="{{ Storage::disk('public')->url($news->image) }}">
+                        @else
                         <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
                             <title>{{ $news->title }}</title>
                             <rect width="100%" height="100%" fill="#55595c"/>
@@ -22,12 +25,13 @@
                             <br><em>{{ $news->category_title }}</em>
                             </text>
                         </svg>
+                        @endif
+
                         <div class="card-body">
                             <p class="card-text">{{ $news->description }}</p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
                                     <a href="{{ route('news.show', ['news' => $news->id]) }}" class="btn btn-sm btn-outline-secondary">Смотреть подробнее</a>
-
                                 </div>
                                 <small class="text-muted">Дата добавления: <br> {{ $news->created_at }}</small>
                             </div>
